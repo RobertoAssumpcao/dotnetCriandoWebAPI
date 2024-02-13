150f35e47a24d18a190da46387ae40c69b35e319
Roberto Assumpção <betoassumpcao@outlook.com>
2024-02-12 22:06:19 -0300

    chore - migration

---
+++ dotnetCriandoWebAPI/Migrations/20240213002939_capacidadeCinema.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetCriandoWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class capacidadeCinema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Capacidade",
+                table: "Cinema",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capacidade",
+                table: "Cinema");
+        }
+    }
+}
